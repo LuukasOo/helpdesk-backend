ba430caa73473de3bddfb3423a826508eda0b602
João Lucas <luckas2k20@gmail.com>
2022-04-22 23:12:14 -0300

    Endpoint findbyId para tecnico

---
+++ src/main/java/com/lucas/helpdesk/resources/TecnicoResource.java
@@ -0,0 +1,27 @@
+package com.lucas.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lucas.helpdesk.domain.Tecnico;
+import com.lucas.helpdesk.service.TecnicoService;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResource {
+	
+	
+	@Autowired
+	private TecnicoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tecnico> findbyId(@PathVariable Integer id){
+		Tecnico obj = service.findbyId(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
