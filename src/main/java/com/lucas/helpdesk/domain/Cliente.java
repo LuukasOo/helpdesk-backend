95bad557287f2dd9f16d700feec8813ac3a3339e
João Lucas <luckas2k20@gmail.com>
2022-04-27 16:22:37 -0300

    CRUD para cliente implementado

--- src/main/java/com/lucas/helpdesk/domain/Cliente.java
+++ src/main/java/com/lucas/helpdesk/domain/Cliente.java
@@ -1,19 +1,20 @@
 package com.lucas.helpdesk.domain;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.lucas.helpdesk.domain.dtos.ClienteDTO;
 import com.lucas.helpdesk.domain.enums.Perfil;
 
 @Entity
-public class Cliente extends Pessoa implements Serializable {
+public class Cliente extends Pessoa {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
@@ -28,6 +29,17 @@ public class Cliente extends Pessoa implements Serializable {
 		addPerfil(Perfil.CLIENTE);
 	}
 
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+
 	public List<Chamado> getChamados() {
 		return chamados;
 	}
@@ -35,6 +47,5 @@ public class Cliente extends Pessoa implements Serializable {
 	public void setChamados(List<Chamado> chamados) {
 		this.chamados = chamados;
 	}
-
 
 }
